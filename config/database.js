42f3958f2f271c63831516269c860a63048ae703
You <you9009@foxmail.com>
2020-09-23 21:22:43 +0800

    init

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'root'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'WEknZSpGCLrA83GS'),
       },
