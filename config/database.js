33bc1932461241bfd94b8b66bc2777cce706b074
You <youux@outlook.com>
2020-09-27 15:09:53 +0800

    修改权限

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'gridsome_api'),
-        username: env('DATABASE_USERNAME', 'gridsome_api'),
+        username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'aytD4eJrPJajd6ki'),
       },
       options: {},
